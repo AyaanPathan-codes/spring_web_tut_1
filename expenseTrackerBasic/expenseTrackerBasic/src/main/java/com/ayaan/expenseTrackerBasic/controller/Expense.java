cccd87702d87e6f8443f98dfe279f4c5448fd738
confirm <ayaanpathanra@gmail.com>
2026-04-11 15:46:29 +0530

    Add Service for Expense

--- expenseTrackerBasic/expenseTrackerBasic/src/main/java/com/ayaan/expenseTrackerBasic/controller/Expense.java
+++ expenseTrackerBasic/expenseTrackerBasic/src/main/java/com/ayaan/expenseTrackerBasic/controller/Expense.java
@@ -2,8 +2,14 @@ package com.ayaan.expenseTrackerBasic.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class Expense {
-
+        private ExpenseService service;
+    public List<Expense> getUsers(){
+         List<Expense> service.getUsers;
+    }
 }
