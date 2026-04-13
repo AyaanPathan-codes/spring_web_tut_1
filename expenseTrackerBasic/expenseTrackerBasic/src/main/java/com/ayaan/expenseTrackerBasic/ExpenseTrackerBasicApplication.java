c9cb8f4e2abcc355a07fa4908ac76ff78b9f78b5
confirm <ayaanpathanra@gmail.com>
2026-04-13 16:03:28 +0530

    Updates

---
+++ expenseTrackerBasic/expenseTrackerBasic/src/main/java/com/ayaan/expenseTrackerBasic/ExpenseTrackerBasicApplication.java
@@ -0,0 +1,13 @@
+package com.ayaan.expenseTrackerBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerBasicApplication.class, args);
+	}
+
+}
