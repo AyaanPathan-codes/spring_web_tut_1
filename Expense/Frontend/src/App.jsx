6c0b22f72ee3eeb486c7b91d783dc9d4eff57fc9
confirm <ayaanpathanra@gmail.com>
2026-04-14 16:09:56 +0530

    Add CORS

--- Expense/Frontend/src/App.jsx
+++ Expense/Frontend/src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Stats from "./pages/Stats";
+import AddExpense from "./pages/AddExpense";
 
 const App = () => {
-
-  
   return (
-    <div></div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/stats" element={<Stats />} />
+        <Route path="/add" element={<AddExpense />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
