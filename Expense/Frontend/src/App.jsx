a5c53294d3ca69e00e15a4bf4b642e96a2a8e28c
confirm <ayaanpathanra@gmail.com>
2026-04-15 15:28:21 +0530

    Add Student Controller

--- Expense/Frontend/src/App.jsx
+++ Expense/Frontend/src/App.jsx
@@ -1,9 +1,9 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
 import Stats from "./pages/Stats";
 import AddExpense from "./pages/AddExpense";
 import Analytics from "./pages/Analytics";
+import AddSavings from "./pages/AddSavings";
 
 const App = () => {
   return (
@@ -12,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Analytics />} />
         <Route path="/stats" element={<Stats />} />
         <Route path="/add" element={<AddExpense />} />
+        <Route path="/add-savings" element={<AddSavings />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ springSecurity/springSecurity/src/main/java/com/ayaan/springSecurity/StdudentController.java
@@ -0,0 +1,4 @@
+package com.ayaan.springSecurity;
+
+public class StdudentController {
+}
